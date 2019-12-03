3094b8b76289cfea131016815d8c70724e8413b3
Ankur Malik <ankur.malik@sap.com>
2019-12-03 06:45:25 +0530

    hi2

--- packages/testapiconsumer2/src/index.ts
+++ packages/testapiconsumer2/src/index.ts
@@ -1,10 +1,13 @@
 import { fStudents } from "./Students";
 
+
 console.log("New Ankur");
 console.log("Ankur");
 console.log("hi");
 console.log("Ankur Malik");
 
 console.log("hi 2");
+
+console.log("hi 3");
 
 export { fStudents };

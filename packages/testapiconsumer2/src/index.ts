64d089d16b7cba98682aff6d73ce2e7d38a91951
Ankur Malik <ankur.malik@sap.com>
2019-12-03 09:35:07 +0530

    Remived all

--- packages/testapiconsumer2/src/index.ts
+++ packages/testapiconsumer2/src/index.ts
@@ -1,12 +1,3 @@
 import { fStudents } from "./Students";
 
-console.log("Old Ankur");
-console.log("Ankur");
-console.log("hi");
-console.log("Ankur Malik");
-
-console.log("hi 2");
-
-console.log("hi 3");
-
 export { fStudents };
